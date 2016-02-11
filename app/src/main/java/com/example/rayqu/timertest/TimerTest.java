20a69d0f18df1e126d89bb4d13b2ab67b82853f3
David Guardiola <David Guardiola>
2016-02-11 17:14:54 -0500

    SecondCommit

--- app/src/main/java/com/example/rayqu/timertest/TimerTest.java
+++ app/src/main/java/com/example/rayqu/timertest/TimerTest.java
@@ -1,9 +1,11 @@
  package com.example.rayqu.timertest;
 
+import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -38,12 +41,13 @@ import java.util.List;
      //double midpoint = 5;
      boolean suspended = false;
 
-     double trueMid;
+     double[] trueMid;
 
      //246.8403042
 
      private ListView lv;
 
+//     protected PowerManager.WakeLock mWakeLock;
 
      MediaPlayer mySound;
 
@@ -75,6 +79,20 @@ import java.util.List;
 
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_timer_test);
+
+         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+         /* will make the screen be always on until this Activity gets destroyed. */
+         //final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+         //this.mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "My Tag");
+         //this.mWakeLock.acquire();
+
+
+
+
+
+
 
 
          mySound = MediaPlayer.create(this, R.raw.soundeffect);
@@ -125,9 +143,16 @@ import java.util.List;
 
          lv.setAdapter(arrayAdapter);
 
-         trueMid = -(Math.log((100/Double.parseDouble(myRegionArray[0][2]))-1))/networking;
+         final double trueMid[] = new double[myNumbers.size()];
 
-         Toast.makeText(TimerTest.this, "" + trueMid , Toast.LENGTH_SHORT).show();
+         for (int i = 0; i < 10; i++)
+         {
+             trueMid[i] = -(Math.log((100 / Double.parseDouble(myRegionArray[0][2])) - 1)) / networking;
+         }
+
+
+
+         Toast.makeText(TimerTest.this, "" + trueMid[0] , Toast.LENGTH_SHORT).show();
 
  //End
 
@@ -150,8 +175,8 @@ import java.util.List;
                                      time.update('s', time.getSec() + (3600 * 12));
                                      time_view.setText(time.toString());
                                      equation_view.setText("Anti-Virus Progress = (Security) * (Panic)(Time) - Shuffle = " + " " + ((security * (panic / 4) * time.getDay()) - shuffle));
-                                     logarithmic_view.setText("This is what the logartithimic will look like for Asia:\n" + (100 / (1 + Math.exp(networking * (time.getDay() - trueMid)))));
-                                     equationVal = 100 / (1 + Math.exp(networking * (time.getDay() - trueMid)));
+                                     logarithmic_view.setText("This is what the logartithimic will look like for Asia:\n" + (100 / (1 + Math.exp(networking * (time.getDay() - trueMid[0])))));
+                                     equationVal = 100 / (1 + Math.exp(networking * (time.getDay() - trueMid[0])));
 
 
                                      if (equationVal >= 50) {
@@ -236,4 +261,13 @@ import java.util.List;
          AppIndex.AppIndexApi.end(client, viewAction);
          client.disconnect();
      }
+
+
+
+
+//     @Override
+//     public void onDestroy() {
+//         this.mWakeLock.release();
+//         super.onDestroy();
+//     }
  }
