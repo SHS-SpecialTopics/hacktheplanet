0403c79a75364e610cfd08779c2e62b2860bfb38
David Guardiola <David Guardiola>
2016-02-18 08:26:07 -0500

    David Feb 18

---
+++ app/src/main/java/com/example/rayqu/timertest/DataOutline.java
@@ -0,0 +1,23 @@
+package com.example.rayqu.timertest;
+
+/** Created by rayqu on 1/8/2016. */
+
+public class DataOutline {
+
+
+        // Labels table name
+        public static final String TABLE = "VirusData";
+
+        // Labels Table Columns names
+        public static final String KEY_ID = "id";
+        public static final String KEY_name = "name";
+        public static final String KEY_category = "category";
+        public static final String KEY_value = "value";
+
+        // property help us to keep data
+        public int virus_ID;
+        public String name;
+        public String category;
+        public int value;
+
+}
