dc821cc27bbbd7cc7443393d42013d6893bfdd5f
ndsoccerstar8598 <ndsoccerstar55@gmail.com>
2016-02-26 09:10:52 -0500

    Add tab functionality Nick 2/26/2016

--- app/src/main/java/com/example/rayqu/timertest/Upgrades.java
+++ app/src/main/java/com/example/rayqu/timertest/Upgrades.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 /**
  * Created by Owner on 2/17/2016.
  */
 public class Upgrades extends AppCompatActivity implements View.OnClickListener {
-    Button[] buttons = new Button[15];
+    Button[] networkButtons = new Button[15];
+    Button[] tabButtons = new Button[3];
+    LinearLayout[] networkingLayouts = new LinearLayout[5];
     boolean infectedPrograms=false;
     boolean LAN =false;
     boolean businessRouters =false;
@@ -25,111 +28,133 @@ public class Upgrades extends AppCompatActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upgrades);
-        buttons[0] = (Button) findViewById(R.id.popUpButton) ;
-        buttons[0].setOnClickListener(this);
-        buttons[1] = (Button) findViewById(R.id.programsButton) ;
-        buttons[1].setOnClickListener(this);
-        buttons[2] = (Button) findViewById(R.id.lanButton) ;
-        buttons[2].setOnClickListener(this);
-        buttons[3] = (Button) findViewById(R.id.usbButton) ;
-        buttons[3].setOnClickListener(this);
-        buttons[4] = (Button) findViewById(R.id.manButton) ;
-        buttons[4].setOnClickListener(this);
-        buttons[5] = (Button) findViewById(R.id.serversTCButton) ;
-        buttons[5].setOnClickListener(this);
-        buttons[6] = (Button) findViewById(R.id.satelliteButton) ;
-        buttons[6].setOnClickListener(this);
-        buttons[7] = (Button) findViewById(R.id.cloudButton) ;
-        buttons[7].setOnClickListener(this);
-        buttons[8] = (Button) findViewById(R.id.mainframeButton) ;
-        buttons[8].setOnClickListener(this);
-        buttons[9] = (Button) findViewById(R.id.serversBuisnessButton) ;
-        buttons[9].setOnClickListener(this);
-        buttons[10] = (Button) findViewById(R.id.atlanticCablesButton) ;
-        buttons[10].setOnClickListener(this);
-        buttons[11] = (Button) findViewById(R.id.homeRoutersButton) ;
-        buttons[11].setOnClickListener(this);
-        buttons[12] = (Button) findViewById(R.id.buisnessRoutersButton) ;
-        buttons[12].setOnClickListener(this);
-        buttons[13] = (Button) findViewById(R.id.cellTowersButton) ;
-        buttons[13].setOnClickListener(this);
-        buttons[14] = (Button) findViewById(R.id.internetProvidersButton) ;
-        buttons[14].setOnClickListener(this);
+        networkButtons[0] = (Button) findViewById(R.id.popUpButton) ;
+        networkButtons[0].setOnClickListener(this);
+        networkButtons[1] = (Button) findViewById(R.id.programsButton) ;
+        networkButtons[1].setOnClickListener(this);
+        networkButtons[2] = (Button) findViewById(R.id.lanButton) ;
+        networkButtons[2].setOnClickListener(this);
+        networkButtons[3] = (Button) findViewById(R.id.usbButton) ;
+        networkButtons[3].setOnClickListener(this);
+        networkButtons[4] = (Button) findViewById(R.id.manButton) ;
+        networkButtons[4].setOnClickListener(this);
+        networkButtons[5] = (Button) findViewById(R.id.serversTCButton) ;
+        networkButtons[5].setOnClickListener(this);
+        networkButtons[6] = (Button) findViewById(R.id.satelliteButton) ;
+        networkButtons[6].setOnClickListener(this);
+        networkButtons[7] = (Button) findViewById(R.id.cloudButton) ;
+        networkButtons[7].setOnClickListener(this);
+        networkButtons[8] = (Button) findViewById(R.id.mainframeButton) ;
+        networkButtons[8].setOnClickListener(this);
+        networkButtons[9] = (Button) findViewById(R.id.serversBuisnessButton) ;
+        networkButtons[9].setOnClickListener(this);
+        networkButtons[10] = (Button) findViewById(R.id.atlanticCablesButton) ;
+        networkButtons[10].setOnClickListener(this);
+        networkButtons[11] = (Button) findViewById(R.id.homeRoutersButton) ;
+        networkButtons[11].setOnClickListener(this);
+        networkButtons[12] = (Button) findViewById(R.id.buisnessRoutersButton) ;
+        networkButtons[12].setOnClickListener(this);
+        networkButtons[13] = (Button) findViewById(R.id.cellTowersButton) ;
+        networkButtons[13].setOnClickListener(this);
+        networkButtons[14] = (Button) findViewById(R.id.internetProvidersButton) ;
+        networkButtons[14].setOnClickListener(this);
 
-        buttons[1].setVisibility(View.INVISIBLE);
-        buttons[2].setVisibility(View.INVISIBLE);
-        buttons[4].setVisibility(View.INVISIBLE);
-        buttons[6].setVisibility(View.INVISIBLE);
-        buttons[7].setVisibility(View.INVISIBLE);
-        buttons[8].setVisibility(View.INVISIBLE);
-        buttons[10].setVisibility(View.INVISIBLE);
-        buttons[12].setVisibility(View.INVISIBLE);
-        buttons[13].setVisibility(View.INVISIBLE);
+        tabButtons[0] = (Button) findViewById(R.id.networkingButton) ;
+        tabButtons[0].setOnClickListener(this);
+        tabButtons[1]= (Button) findViewById(R.id.lethalityButton) ;
+        tabButtons[1].setOnClickListener(this);
+        tabButtons[2]=(Button) findViewById(R.id.securityButton) ;
+        tabButtons[2].setOnClickListener(this);
+
+        networkingLayouts[0]= (LinearLayout) findViewById(R.id.firstRowLinearLayout) ;
+        networkingLayouts[1]= (LinearLayout) findViewById(R.id.secondRowlinearLayout) ;
+        networkingLayouts[2]= (LinearLayout) findViewById(R.id.thirdRowlinearLayout) ;
+        networkingLayouts[3]= (LinearLayout) findViewById(R.id.fourthRowLinearLayout) ;
+        networkingLayouts[4]= (LinearLayout) findViewById(R.id.fifthRowLinearLayout) ;
+
+        networkButtons[1].setVisibility(View.INVISIBLE);
+        networkButtons[2].setVisibility(View.INVISIBLE);
+        networkButtons[4].setVisibility(View.INVISIBLE);
+        networkButtons[6].setVisibility(View.INVISIBLE);
+        networkButtons[7].setVisibility(View.INVISIBLE);
+        networkButtons[8].setVisibility(View.INVISIBLE);
+        networkButtons[10].setVisibility(View.INVISIBLE);
+        networkButtons[12].setVisibility(View.INVISIBLE);
+        networkButtons[13].setVisibility(View.INVISIBLE);
     }
     public void onClick(View v) {
-        if(v==buttons[0]){
-            buttons[1].setVisibility(View.VISIBLE);
+        if(v==networkButtons[0]){
+            networkButtons[1].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[1]) {
+        if(v==networkButtons[1]) {
             infectedPrograms=true;
             if(LAN)
-                buttons[4].setVisibility(View.VISIBLE);
+                networkButtons[4].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[2]) {
+        if(v==networkButtons[2]) {
             LAN=true;
             if(infectedPrograms)
-                buttons[4].setVisibility(View.VISIBLE);
+                networkButtons[4].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[3]){
-            buttons[2].setVisibility(View.VISIBLE);
+        if(v==networkButtons[3]){
+            networkButtons[2].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[4]){
+        if(v==networkButtons[4]){
             MAN=true;
             if(satellites && mainframes && cables){
-                buttons[7].setVisibility(View.VISIBLE);
+                networkButtons[7].setVisibility(View.VISIBLE);
             }
         }
-        if(v==buttons[5]){
-            buttons[6].setVisibility(View.VISIBLE);
+        if(v==networkButtons[5]){
+            networkButtons[6].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[6]){
+        if(v==networkButtons[6]){
             satellites=true;
             if(MAN && mainframes && cables){
-                buttons[7].setVisibility(View.VISIBLE);
+                networkButtons[7].setVisibility(View.VISIBLE);
             }
         }
-        if(v==buttons[8]){
+        if(v==networkButtons[8]){
             mainframes=true;
             if(MAN && satellites && cables){
-                buttons[7].setVisibility(View.VISIBLE);
+                networkButtons[7].setVisibility(View.VISIBLE);
             }
         }
-        if(v==buttons[9]){
-            buttons[8].setVisibility(View.VISIBLE);
+        if(v==networkButtons[9]){
+            networkButtons[8].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[10]){
+        if(v==networkButtons[10]){
             cables=true;
             if(MAN && satellites && mainframes){
-                buttons[7].setVisibility(View.VISIBLE);
+                networkButtons[7].setVisibility(View.VISIBLE);
             }
         }
-        if(v==buttons[11]){
-            buttons[12].setVisibility(View.VISIBLE);
+        if(v==networkButtons[11]){
+            networkButtons[12].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[14]){
-            buttons[13].setVisibility(View.VISIBLE);
+        if(v==networkButtons[14]){
+            networkButtons[13].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[12]) {
+        if(v==networkButtons[12]) {
             businessRouters=true;
             if(cellTowers)
-                buttons[10].setVisibility(View.VISIBLE);
+                networkButtons[10].setVisibility(View.VISIBLE);
         }
-        if(v==buttons[13]) {
+        if(v==networkButtons[13]) {
             cellTowers=true;
             if(businessRouters)
-                buttons[10].setVisibility(View.VISIBLE);
+                networkButtons[10].setVisibility(View.VISIBLE);
         }
-
+        if(v==tabButtons[1]){
+            for(LinearLayout l: networkingLayouts){
+                l.setVisibility(View.GONE);
+            }
+        }
+        if(v==tabButtons[0]){
+            for(LinearLayout l: networkingLayouts){
+                l.setVisibility(View.VISIBLE);
+            }
+        }
     }
     public static double getNetworking(){
         return networking;
