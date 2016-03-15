2d95d79e29ab3a4aa4b724b1855d33ce80459685
ndsoccerstar8598 <ndsoccerstar55@gmail.com>
2016-03-15 09:33:00 -0400

    Nick 3/15/2016

--- app/src/main/java/com/example/rayqu/timertest/lethality_upgrade.java
+++ app/src/main/java/com/example/rayqu/timertest/lethality_upgrade.java
@@ -22,6 +22,8 @@ public class lethality_upgrade extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final boolean[] mainframes = {false};
+        final boolean[] drones = {false};
         final View view = inflater.inflate(R.layout.fragment_lethality_upgrade, container, false);
 
         view.findViewById(R.id.LaptopButton).setOnClickListener(new View.OnClickListener() {
@@ -30,7 +32,127 @@ public class lethality_upgrade extends Fragment {
                 view.findViewById(R.id.DesktopButton).setVisibility(View.VISIBLE);
                 view.findViewById(R.id.LaptopButton).setClickable(false);
             }
-        }); ;
+        });
+        view.findViewById(R.id.DesktopButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.StationsButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.DesktopButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.StationsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.MobilePhonesButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.StationsButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.MobilePhonesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.SmartPhonesButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.MobilePhonesButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.SmartPhonesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.TablesButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.SmartPhonesButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.TablesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.UCCButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.TablesButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.GovernmentSatellitesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.WifiScrammbleButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.RovButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.TablesButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.WifiScrammbleButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.RandomRebootsButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.WifiScrammbleButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.RovButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.WarsuitsButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.RovButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.WarsuitsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.DronesButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.WarsuitsButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.TablesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.UCCButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.TablesButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.RandomRebootsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.ServersButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.RandomRebootsButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.ServersButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.Mainframe1Button).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.ServersButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.DronesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drones[0]=true;
+                if(mainframes[0]){
+                    view.findViewById(R.id.LaunchCodesButton).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        view.findViewById(R.id.Mainframe1Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainframes[0]=true;
+                if(drones[0]){
+                    view.findViewById(R.id.LaunchCodesButton).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        if(savedInstanceState == null) {
+            view.findViewById(R.id.DesktopButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.StationsButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.MobilePhonesButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.SmartPhonesButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.TablesButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.UCCButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.WifiScrammbleButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.LaunchCodesButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.RovButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.RandomRebootsButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.ServersButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.Mainframe1Button).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.DronesButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.WarsuitsButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.RovButton).setVisibility(View.INVISIBLE);
+        }
         return view;
     }
 
--- app/src/main/java/com/example/rayqu/timertest/security_upgrade.java
+++ app/src/main/java/com/example/rayqu/timertest/security_upgrade.java
@@ -23,7 +23,114 @@ public class security_upgrade extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_security_upgrade, container, false);
+        final View view = inflater.inflate(R.layout.fragment_security_upgrade, container, false);
+
+        view.findViewById(R.id.IButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.IIButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.IButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.IIButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.IIIButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.IIButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.IIIButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.IVButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.IIIButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.IVButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.VButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.IVButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.VButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.VButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.CryBabyButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.ToddlersTantrumButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.CryBabyButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.ToddlersTantrumButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.TeenAngstButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.ToddlersTantrumButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.TeenAngstButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.ParentsScornButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.TeenAngstButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.ParentsScornButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.ProgrammersRageButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.ParentsScornButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.ProgrammersRageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.GodsWrathButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.ProgrammersRageButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.MacroVirusButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.FileInfectorButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.MacroVirusButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.FileInfectorButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.OverwriteButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.FileInfectorButton).setClickable(false);
+            }
+        });
+        view.findViewById(R.id.OverwriteButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.findViewById(R.id.ResidentVirusButton).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.OverwriteButton).setClickable(false);
+            }
+        });
+
+        if(savedInstanceState == null) {
+            view.findViewById(R.id.IIButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.IIIButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.IVButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.VButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.ToddlersTantrumButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.TeenAngstButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.ParentsScornButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.ProgrammersRageButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.GodsWrathButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.FileInfectorButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.OverwriteButton).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.ResidentVirusButton).setVisibility(View.INVISIBLE);
+        }
+        return view;
     }
 
 }
