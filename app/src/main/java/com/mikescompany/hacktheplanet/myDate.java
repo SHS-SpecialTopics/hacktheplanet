48191883eb70e97f17fc75a6a83953d47810f67d
David Guardiola <David Guardiola>
2016-06-09 08:25:02 -0400

    Fixed Merge Errors 6/9/16

--- app/src/main/java/com/mikescompany/hacktheplanet/myDate.java
+++ app/src/main/java/com/mikescompany/hacktheplanet/myDate.java
@@ -1,72 +1,72 @@
-<<<<<<< HEAD
-package com.mikescompany.hacktheplanet;
-
-/**
- * Created by rayqu on 2/3/2016.
- */
-public class myDate {
-    private long hr;
-    private long day;
-    private long min;
-    private long sec;
-    private long yr;
-
-    private long time;
-
-    public myDate(long l) {
-        sec = l;
-        set(sec);
-        time = 0;
-    }
-
-    //changed following to time instead of day
-    public String toString() {
-        return ("year_" + yr + "\nday_"  + day + "\ntime_" + time);
-    }
-
-    public void update(/* char c, long t*/) {
-//        if(c=='s')
-//            sec = t;
+//<<<<<<< HEAD
+//package com.mikescompany.hacktheplanet;
 //
+///**
+// * Created by rayqu on 2/3/2016.
+// */
+//public class myDate {
+//    private long hr;
+//    private long day;
+//    private long min;
+//    private long sec;
+//    private long yr;
+//
+//    private long time;
+//
+//    public myDate(long l) {
+//        sec = l;
 //        set(sec);
-
-        //time++;
-
-        set(time++);
-    }
-
-    private void set(long m) {
-
-//        min = sec / 60;
-//        hr = min / 60;
-        day = time; //hr / 24;
-        yr = day/364;
-//        if (min > 59)//hours
-//            min -= hr * 60;
-//        if (hr > 23)//days
-//            hr -= day * 24;
-        if(day>363) {//years
-            day -= yr * 364;
-            time= time -1;
-        }
-
-    }
-
-    public long getSec() {
-        return sec;
-    }
-
-    public long getDay() {
-        return day;
-    }
-
-    public long getTime() {
-        return time;
-    }
-}
-
-||||||| merged common ancestors
-=======
+//        time = 0;
+//    }
+//
+//    //changed following to time instead of day
+//    public String toString() {
+//        return ("year_" + yr + "\nday_"  + day + "\ntime_" + time);
+//    }
+//
+//    public void update(/* char c, long t*/) {
+////        if(c=='s')
+////            sec = t;
+////
+////        set(sec);
+//
+//        //time++;
+//
+//        set(time++);
+//    }
+//
+//    private void set(long m) {
+//
+////        min = sec / 60;
+////        hr = min / 60;
+//        day = time; //hr / 24;
+//        yr = day/364;
+////        if (min > 59)//hours
+////            min -= hr * 60;
+////        if (hr > 23)//days
+////            hr -= day * 24;
+//        if(day>363) {//years
+//            day -= yr * 364;
+//            time= time -1;
+//        }
+//
+//    }
+//
+//    public long getSec() {
+//        return sec;
+//    }
+//
+//    public long getDay() {
+//        return day;
+//    }
+//
+//    public long getTime() {
+//        return time;
+//    }
+//}
+//
+//||||||| merged common ancestors
+//=======
 package com.mikescompany.hacktheplanet;
 
 /**
@@ -132,5 +132,3 @@ public class myDate {
         return time;
     }
 }
-
->>>>>>> david/master
